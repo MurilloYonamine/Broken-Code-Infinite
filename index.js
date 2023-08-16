a1441a3819c4dcab9d6f9653c0433b86fd74dd4f
MurilloYonamine <picodaplantadearroz@gmail.com>
2023-08-16 17:18:00 -0300

    MUDEI TUDO

--- index.js
+++ index.js
@@ -22,11 +22,20 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.post('/cadastro/usuarios', (req, res) => {
-    const nome = req.body.nome;
-    const senha = req.body.senha;
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'bdjogoti93'
+});
 
-    const sql = `INSERT INTO tb_usuarios (Nome, Senha) VALUES('${nome}', '${senha}')`;
+// CADASTRO DOS USUARIOS
+app.post('/cadastro/usuarios', (req, res) => {
+    const nome = req.body.cnome;
+    const nick = req.body.cnick;
+    const senha = req.body.csenha;
+
+    const sql = `INSERT INTO tb_usuarios (Nome, Nickname, Senha) VALUES('${nome}', '${nick}', '${senha}')`;
 
     conn.query(sql, (err) => {
         if (err) {
@@ -37,11 +46,27 @@ app.post('/cadastro/usuarios', (req, res) => {
     });
 });
 
-const conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'bdjogoti93'
+// LOGIN DO USUARIO
+app.post('/login/', (req, res) => {
+    const nick = req.body.nick;
+    const senha = req.body.senha;
+
+    const sql = `SELECT * FROM tb_usuarios WHERE Nickname = '${nick}' and Senha = '${senha}'`;
+    conn.query(sql, (err, data) => {
+        if (err) {
+            console.log(err);
+        }
+
+        if (data.length > 0) {
+            console.log('Deu certo!');
+            res.render('login', { data });
+            console.log(data);
+            return;
+        } else {
+            alert("Não deu certo!");
+            return;
+        }
+    });
 });
 
 conn.connect((err) => {
