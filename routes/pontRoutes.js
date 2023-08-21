3618a33a11af1c337feeffe76c6b666f068122bb
MurilloYonamine <picodaplantadearroz@gmail.com>
2023-08-21 16:55:25 -0300

    Arrumei o erro do git

--- routes/pontRoutes.js
+++ routes/pontRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const PontController = require('../controllers/PontController');
 
-router.get('/folders/rank', PontController.mostrarPontuacao)
+router.get('/', PontController.mostrarPontuacao);
 
 module.exports = router;
