e8dc62e8fc34f56875c654aac2f6d068ce6c40df
MURILLO GOMES YONAMINE <murillo.gyonamine@senacsp.edu.br>
2023-08-25 16:19:52 -0300

    Arrumei as autenticação

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -4,49 +4,52 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 
 module.exports = class AuthController {
-    static async loginPost(req, res) {
-        const { nick, senha } = req.body;
 
-        // encontrar o usuário
-        const user = await User.findOne({ where: { UsNickname: nick } });
+  static async loginPost(req, res) {
+    const { nick, senha } = req.body;
 
-        if (!user) {
-            res.render('home', { erroLogin: "Nick não encontrado!" });
-            return;
-        }
+    // encontrar o usuário
+    const user = await User.findOne({ where: { UsNickname: nick } });
 
-        // checar se a senha bate
-        const passwordMatch = bcrypt.compareSync(senha, user.UsSenha);
-
-        if (!passwordMatch) {
-            res.render('home', { erroLogin: "Senha incorreta!" });
-            return;
-        }
-
-        //Inicialização sessão
-
-        res.render('home', { loginMensagem: 'Login realizado com sucesso!' });
+    if (!user) {
+      res.render("home", { erroLogin: "Nick não encontrado!" });
+      return;
     }
 
-    static async registrarPost(req, res) {
-        const { cnome, cnick, csenha } = req.body;
+    // checar se a senha bate
+    const passwordMatch = bcrypt.compareSync(senha, user.UsSenha);
 
-        const salt = bcrypt.genSaltSync(2);
-        const senhaCriptograda = bcrypt.hashSync(csenha, salt);
-
-        const user = {
-            UsNome: cnome,
-            UsNickname: cnick,
-            UsSenha: senhaCriptograda
-        };
-        try {
-            await User.create(user);
-            res.render('home', { registrarMensagem: 'Usuário criado com sucesso!' });
-        } catch (err) {
-            const ChecarSeNickExistir = await User.findOne({ where: { UsNickname: cnick } });
-            if (ChecarSeNickExistir) {
-                res.render('home', { erroCadastro: 'Nickname já existe!' });
-            }
-        }
+    if (!passwordMatch) {
+      res.render("home", { erroLogin: "Senha incorreta!" });
+      return;
     }
+
+    //Inicialização sessão
+
+    res.render("home", { loginMensagem: "Login realizado com sucesso!" });
+  }
+
+  static async registrarPost(req, res) {
+    const { cnome, cnick, csenha } = req.body;
+
+    const salt = bcrypt.genSaltSync(2);
+    const senhaCriptograda = bcrypt.hashSync(csenha, salt);
+
+    const user = {
+      UsNome: cnome,
+      UsNickname: cnick,
+      UsSenha: senhaCriptograda,
+    };
+    try {
+      await User.create(user);
+      res.render("home", { registrarMensagem: "Usuário criado com sucesso!" });
+    } catch (err) {
+      const ChecarSeNickExistir = await User.findOne({
+        where: { UsNickname: cnick },
+      });
+      if (ChecarSeNickExistir) {
+        res.render("home", { erroCadastro: "Nickname já existe!" });
+      }
+    }
+  }
 };
--- index.js
+++ index.js
@@ -1,55 +1,57 @@
-const express = require('express');
+const express = require("express");
 const exphbs = require("express-handlebars");
 
-const path = require('path');
+const path = require("path");
 
 const app = express();
 
 const conn = require("./db/conn");
 
 // Models
-const user = require('./models/User');
-const pontuacao = require('./models/Pontuacao');
+const user = require("./models/User");
+const pontuacao = require("./models/Pontuacao");
 
 // Import Routes
 const authRoutes = require("./routes/authRoutes");
 const pontRoutes = require("./routes/pontRoutes");
 const homeRoutes = require("./routes/homeRoutes");
 
-
 // Template engine
-app.engine('handlebars', exphbs.engine({
-    extname: 'handlebars',
-    defaultLayout: 'layouts/main',
-    layoutsDir: path.join(__dirname, 'views/'),
-    partialsDir: [
-        path.join(__dirname, 'views/'),
-    ]
-}));
+app.engine(
+  "handlebars",
+  exphbs.engine({
+    extname: "handlebars",
+    defaultLayout: "layouts/main",
+    layoutsDir: path.join(__dirname, "views/"),
+    partialsDir: [path.join(__dirname, "views/")],
+  })
+);
 
-app.set('view engine', 'handlebars');
+app.set("view engine", "handlebars");
 
-app.set('views', path.join(__dirname, 'views'));
+app.set("views", path.join(__dirname, "views"));
 
 // receber resposta do body
 app.use(
-    express.urlencoded({
-        extended: true
-    })
+  express.urlencoded({
+    extended: true,
+  })
 );
 
 app.use(express.json());
 
 //public path
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 //Routes
 
-app.use('/', authRoutes);
-app.use('/', pontRoutes);
-app.use('/', homeRoutes);
+app.use("/", authRoutes);
+app.use("/", pontRoutes);
+app.use("/", homeRoutes);
 
-
-conn.sync(/*{ force: true }*/).then(() => {
+conn
+  .sync(/*{ force: true }*/)
+  .then(() => {
     app.listen(3000);
-}).catch((err) => console.log(err));;
+  })
+  .catch((err) => console.log(err));
