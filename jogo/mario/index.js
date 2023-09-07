06121830712b4db7408fd8f6e92752eb823eb414
GUSTAVO ARAUJO DE SOUZA <gustavo.asouza18@senacsp.edu.br>
2023-09-07 18:42:30 -0300

    colisao

--- jogo/mario/index.js
+++ jogo/mario/index.js
@@ -1,5 +1,12 @@
 const kai= document.querySelector(".kai")
 const pc= document.querySelector(".pc")
+const arvores= document.querySelector(".fundo")
+const nuvem= document.querySelector(".clouds")
+const janela= document.querySelector(".janela")
+const janela2= document.querySelector(".janela2")
+const chao= document.querySelector(".footer")
+const chao2= document.querySelector(".footer2")
+
 
 
 
@@ -13,17 +20,24 @@ function pular(){
 }
 
 // Precisa terminar de configurar
-const loop = setTimeout(()=>{
+const loop = setInterval(()=>{
  const pipPosition= pc.offsetLeft; //pegando o afastamento da esquerda
 //Para pegar qualquer propriedade do css fazemos
 //o sinal de mais converte para numeros
 const kaiPosition= +window.getComputedStyle(kai).bottom.replace("px","")
 console.log(kaiPosition)
 console.log(pipPosition)
- if(pipPosition<=120 && pipPosition>0 && kaiPosition<80){
+ if(pipPosition<=250 && pipPosition>0 && kaiPosition<80){
 
     pc.style.animation ="none";
     pc.style.left=`${pipPosition}px`
+    arvores.style.animation ="none";
+    nuvem.style.animation ="none";
+    janela.style.animation ="none";
+    janela2.style.animation ="none";
+    chao.style.animation ="none";
+    chao2.style.animation ="none";
+
 }
 
 },10)
