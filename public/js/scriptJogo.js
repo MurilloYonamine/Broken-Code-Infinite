5b83e27806633f87a45c9274395631c3ec66ee80
guatandre <andreguarido01@gmail.com>
2023-09-11 16:36:07 -0300

    fundo sobre

--- public/js/scriptJogo.js
+++ public/js/scriptJogo.js
@@ -80,7 +80,7 @@ const loop = setInterval(() => {
     }
     
     
-
+    //colisao e fim de jogo
     if (pipPosition <= 280 && pipPosition > 10 && kaiPosition < 80) {
 
 
@@ -106,6 +106,9 @@ const loop = setInterval(() => {
 
         kai.style.animation = 'none';
         kai.style.bottom = `${kaiPosition}px`;
+
+
+
         
 
 
